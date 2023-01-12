ca3e4a948c56ccc1d5bc8b7fdc1dc49268aa501a
miguel-dev01 <122533153+miguel-dev01@users.noreply.github.com>
2023-01-12 13:30:59 +0100

    Add files via upload

---
+++ src/Clases/User.java
@@ -0,0 +1,13 @@
+package Clases;
+
+public class User{
+	
+	public String username;
+	public String name;
+	public String nif;
+	public String email;
+	public String address;
+	public String birthdate;
+	public String role;
+	
+}
