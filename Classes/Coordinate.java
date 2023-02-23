5d5e679f3dceac054724adf746a153911c1a3c71
Miguel <122533153+miguel-dev01@users.noreply.github.com>
2023-02-23 20:19:39 +0100

    Add files via upload

---
+++ Classes/Coordinate.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class Coordinate {
+	
+	public int i;
+	public int j;
+	public int direction;
+	
+	// constructor clase Coordinate
+	public Coordinate(int i, int j, int direction) { 
+		
+		this.i = i;
+		this.j = j;
+		this.direction = direction;
+		
+	}
+	
+	
+} // cierre Clase Coordinate
