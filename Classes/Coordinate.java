083d1720804a9e4fbca3c57ad3cf19dc6ea24b57
Miguel <122533153+miguel-dev01@users.noreply.github.com>
2023-03-31 22:28:38 +0200

    Add files via upload

---
+++ Classes/Coordinate.java
@@ -0,0 +1,37 @@
+package Classes;
+
+public class Coordinate {
+	
+	public int i;
+	public int j;
+	public int direction;
+	
+	public Coordinate () {} // Constructor vacio
+	
+	// Constructor para usar en los casos donde no sea necesario usar la direccion y tan solo las coordenadas
+	public Coordinate(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	
+	// Constructor clase Coordinate
+	public Coordinate(int i, int j, int direction) { 
+		
+		this.i = i;
+		this.j = j;
+		this.direction = direction;
+		
+	}
+	
+	// Algunos metodos adicionales
+	public int getI() {
+		return i;
+	}
+	
+	public int getJ() {
+		return j;
+	}
+	
+	
+	
+} // cierre Clase Coordinate
